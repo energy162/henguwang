0e3f10dce3ae65f380d3c05b2a5754d6ad85de89
energy162 <79636113@qq.com>
2015-07-15 07:57:58 +0800

    UPDATE

--- App/Common/common.php
+++ App/Common/common.php
@@ -170,3 +170,70 @@ function ismobile() {
     }
     return false;
  }
+
+function p5wFile($url)
+{
+    $c = file($url);
+    return implode('', $c);
+}
+
+/**
+ * @param $mixed
+ * @return bool
+ * var_export( isint( '123' ) ); //This will return true
+ * var_export( isint( 123 ) ); //This will return true
+ * var_export( isint( 'asd' ) ); //This will return false
+ * var_export( isint( '123asd123' ) ); //This will return false
+ */
+function isint( $mixed )
+{
+    return ( preg_match( '/^\d*$/'  , $mixed) == 1 );
+}
+
+/**
+ * @param $int
+ * @return bool
+ * The above returns:
+ * 155        TRUE
+ * 15.5       FALSE
+ * "155"      TRUE
+ * "15.5"     FALSE
+ * "0155"     TRUE
+ * "I'm 155"  FALSE
+ * "test"     FALSE
+ * ""         FALSE
+ */
+function isint2($int){
+
+    // First check if it's a numeric value as either a string or number
+    if(is_numeric($int) === TRUE){
+
+        // It's a number, but it has to be an integer
+        if((int)$int == $int){
+
+            return TRUE;
+
+            // It's a number, but not an integer, so we fail
+        }else{
+
+            return FALSE;
+        }
+
+        // Not a number
+    }else{
+
+        return FALSE;
+    }
+}
+
+/**
+ * @param $input
+ * @return bool
+ * check if input is a valid number
+ * first number must be 1 thru 9, followed by a number 0-9, no decimals
+ * true for "1", "1000"
+ * false for "01", "-1", "1.2"
+ */
+function isInteger($input){
+    return preg_match('@^[1-9][0-9]*$@',$input) === 1;
+}
